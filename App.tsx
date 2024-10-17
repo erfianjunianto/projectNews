0ef63c14126036415bd33f89b9954a9137552e75
Notiva <notiva@Notivas-MacBook-Pro.local>
2024-10-17 15:29:56 +0700

    update pertemuan 3

--- App.tsx
+++ App.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, Button} from 'react-native';
 import Halaman1 from './src/Halaman1';
 import Halaman2 from './src/Halaman2';
 import Layout from './src/Layout';
 import FlexBasic from './src/FlexBasic';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
 
 const App1 = () => {
+  const [count, setCount] = useState(0);
   return(
     <View>
       <Text style={{fontSize:50}}>Hello ARS</Text>
       <Halaman1 nama_depan="Erfian" nama_belakang="Junianto"/>
+      <Text style={{fontSize:30}}>You clicked {count} times</Text>
+      <Button
+        onPress={() => setCount(count + 1)}
+        title="Click me!"
+      />
     </View>
   );
 }
@@ -25,4 +34,34 @@ const App2 = () => {
   );
 }
 
+const HomeScreen = ({navigation}) => {
+  return (
+    <Button
+      title="Go to Jane's profile"
+      onPress={() =>
+        navigation.navigate('Profile', {name: 'Jane'})
+      }
+    />
+  );
+};
+const ProfileScreen = ({navigation, route}) => {
+  return <Text>This is {route.params.name}'s profile</Text>;
+};
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'Welcome'}}
+        />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 export default App1;
