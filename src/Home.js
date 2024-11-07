4e3f8e9e997fa325f4afa8361afe540485e306ed
Notiva <notiva@Notivas-MacBook-Pro.local>
2024-11-07 15:15:45 +0700

    update them gluestack and public api

--- src/Home.js
+++ src/Home.js
@@ -1,48 +1,108 @@
 import React, {useEffect, useState} from 'react';
-import {ActivityIndicator, FlatList, Text, View} from 'react-native';
+import { GluestackUIStyledProvider, Button, ButtonText } from '@gluestack-ui/themed';
+import { config } from '@gluestack-ui/config';
+import { GluestackUIProvider, Alert, FlatList, AlertText, Card, Image, Text, Heading, Link, HStack, LinkText, } from '@gluestack-ui/themed';
+
+
+
+const Headline = () =>{
+    const [data, setData] = useState([]);
+
+    const getMovies = async () => {
+        try {
+        const response = await fetch('https://newsapi.org/v2/everything?q=bitcoin&pageSize=10&apiKey=6d4b261c262e4a51a4408f2e839d5530');
+        const json = await response.json();
+        setData(json.articles);
+        } catch (error) {
+        console.error(error);
+        } finally {
+        // setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        getMovies();
+    }, []);
+    
+    return (
+        <FlatList
+            data={data}
+            renderItem={({ item }) => (
+            <Card p="$5" borderRadius="$lg" m="$3">
+                <Image
+                mb="$6"
+                h={240}
+                width="$full"
+                borderRadius="$md"
+                source={{
+                uri: 'https://media.wired.com/photos/6703eb3979f13fda7f04485b/191:100/w_1280,c_limit/Satoshi-Nakamoto-biz-1341874258.jpg',
+                }}
+            />
+            <Text
+                fontSize="$sm"
+                fontStyle="normal"
+                fontFamily="$heading"
+                fontWeight="$normal"
+                lineHeight="$sm"
+                mb="$2"
+                sx={{
+                color: '$textLight700',
+                _dark: {
+                    color: '$textDark200',
+                },
+                }}
+            >
+                {item.publishedAt}
+            </Text>
+            <Heading size="md" fontFamily="$heading" mb="$4">
+                {item.title}
+            </Heading>
+            <Link href={item.url} isExternal>
+                <HStack alignItems="center">
+                <LinkText
+                    size="sm"
+                    fontFamily="$heading"
+                    fontWeight="$semibold"
+                    color="$primary600"
+                    $dark-color="$primary300"
+                    textDecorationLine="none"
+                >
+                    Read Blog
+                </LinkText>
+                {/* <Icon
+                    as={ArrowRightIcon}
+                    size="sm"
+                    color="$primary600"
+                    mt="$0.5"
+                    ml="$0.5"
+                    $dark-color="$primary300"
+                /> */}
+                </HStack>
+            </Link>
+            </Card>
+        )}
+        // keyExtractor={(item) => item.id}
+      />
+);
+}
 
 const Home = () => {
-  const [isLoading, setLoading] = useState(true);
-  const [data, setData] = useState([]);
-
-  const getMovies = async () => {
-    try {
-      const response = await fetch('https://reactnative.dev/movies.json');
-      const json = await response.json();
-      setData(json.movies);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    getMovies();
-  }, []);
-
   return (
-    <View style={{flex: 1, padding: 24}}>
-      {isLoading ? (
-        <ActivityIndicator />
-      ) : (
-        <FlatList
-          data={data}
-          keyExtractor={({id}) => id}
-          renderItem={({item}) => (
-            <View>
-            <Text style={{fontSize:50,}}>
-              {item.id},{item.title}, 
-            </Text>
-            <Text style={{fontSize:25,color:'red'}}>
-              {item.releaseYear}
-            </Text>
-            </View>
-          )}
-        />
-      )}
-    </View>
-  );
-};
+    <GluestackUIProvider config={config}>
+      {/* <Button>
+        <ButtonText>Hello World</ButtonText>
+      </Button> */}
+
+        <Alert mx="$2.5" action="info" variant="outline">
+        {/* <AlertIcon as={InfoIcon} mr="$3" /> */}
+            <AlertText>
+                We have updated our terms of service. Please review and accept to continue
+                using our service.
+            </AlertText>
+        </Alert>
+        <Headline />
+    </GluestackUIProvider>
+  )
+}
 
 export default Home;
